498401d0580c8a1920b30c9c051a086d79f24ece
pzgyuanf@gmail.com <pzgyuanf@gmail.com@d29f4a22-43ba-bff7-ab16-927db90db0ac>
2011-02-13 08:59:54 +0000

    comment updated

--- trunk/hjpetstore2.0/src/main/java/org/pprun/hjpetstore/web/rest/HjpetstoreController.java
+++ trunk/hjpetstore2.0/src/main/java/org/pprun/hjpetstore/web/rest/HjpetstoreController.java
@@ -41,7 +41,7 @@ public class HjpetstoreController extends BaseController {
      * <p>
      * For example: user pprun <br />
      * {@code 
-     * curl -H 'Content-Type: application/xml' -H 'Accept: application/xml' -H 'Date: Fri 2011-02-11 19:10:46 UTC' 'http://localhost:8080/hjpetstore/rest/products/dog?apiKey=e4fae4f09fd3b2e6201b7b213d4deae7&signature=zVe5WbWJuJuPQpFLJVpJ4XMYTThdROf5iaU76zdWLweeKvGSLBBJTAce4BayNH07x3poa8gHsIxLkIpFXsd5OQ%3D%3D&page=1&max=100'
+     * curl -H 'Content-Type: application/xml' -H 'Accept: application/xml' -H 'Date: Fri 2011-02-11 19:10:46 UTC' 'http://localhost:8080/hjpetstore/rest/products/{keyword}?apiKey=e4fae4f09fd3b2e6201b7b213d4deae7&signature=zVe5WbWJuJuPQpFLJVpJ4XMYTThdROf5iaU76zdWLweeKvGSLBBJTAce4BayNH07x3poa8gHsIxLkIpFXsd5OQ%3D%3D&page=1&max=100'
      * }
      *
      * @param keyword
